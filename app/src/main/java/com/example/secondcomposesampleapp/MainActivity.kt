f0cad2f7585e8b5ee58e4cfae5c0550e0fe7524b
HamidReza Farahani <h.farahani>
2023-08-23 16:12:15 +0430

    change color and padding

--- app/src/main/java/com/example/secondcomposesampleapp/MainActivity.kt
+++ app/src/main/java/com/example/secondcomposesampleapp/MainActivity.kt
@@ -1,15 +1,18 @@
 package com.example.secondcomposesampleapp
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.secondcomposesampleapp.ui.theme.SecondComposeSampleAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,13 +34,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(2.dp)
+    ) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(8.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview("Light Mode", showBackground = true)
+@Preview("Dark Mode", uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
 fun GreetingPreview() {
     SecondComposeSampleAppTheme {
