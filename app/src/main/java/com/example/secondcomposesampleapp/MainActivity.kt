e2a53ce171c95dc46b214300689177eac032736f
HamidReza Farahani <h.farahani>
2023-08-23 16:54:13 +0430

    handle on click event on button

--- app/src/main/java/com/example/secondcomposesampleapp/MainActivity.kt
+++ app/src/main/java/com/example/secondcomposesampleapp/MainActivity.kt
@@ -7,13 +7,16 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -32,6 +35,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var expanded by rememberSaveable {
+        mutableStateOf(false)
+    }
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 2.dp, horizontal = 4.dp)
@@ -45,8 +51,8 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                     text = "$name!",
                 )
             }
-            ElevatedButton(onClick = { /*TODO*/ }) {
-                Text(text = "Show more")
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
 
